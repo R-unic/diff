f2df60e3998aac47a6a83f88ba3fb7bb1ae826a0
R-unic <roleyspersonal@gmail.com>
2026-05-10 02:11:42 -0400

    test: all of createDiff()

---
+++ tests/src/create-diff.spec.ts
@@ -0,0 +1,140 @@
+import { Assert, Fact } from "@rbxts/runit";
+import { createDiff, type Diff } from "@rbxts/diff";
+
+class CreateDiffTest {
+  @Fact
+  public "non-table objects error"(): void {
+    Assert.throws(() => createDiff(1 as never, 2 as never), "attempt to create diff of non-table objects");
+  }
+
+  @Fact
+  public "shallow array changes"(): void {
+    const a = [69];
+    const b = [420];
+    const diff = createDiff(a, b);
+    Assert.undefined(diff.removed);
+    Assert.defined(diff.changed);
+    Assert.defined(diff.changed[0]);
+    Assert.equal(420, diff.changed[0]);
+  }
+
+  @Fact
+  public "shallow array removals"(): void {
+    const a = [69];
+    const b: number[] = [];
+    const diff = createDiff(a, b);
+    Assert.undefined(diff.changed);
+    Assert.defined(diff.removed);
+    Assert.defined(diff.removed[0 + 1]);
+    Assert.true(diff.removed[0 + 1]);
+  }
+
+  @Fact
+  public "deep array changes"(): void {
+    type T = [number, [[number, number[]]]];
+    const a: T = [69, [[1000, [420]]]];
+    const b: T = [69, [[1000, [1337]]]];
+    const diff = createDiff(a, b);
+    Assert.undefined(diff.removed);
+    Assert.defined(diff.changed);
+    Assert.undefined(diff.changed[0])
+
+    const shallowArrayChange = diff.changed[1] as Diff<T[1]>["changed"];
+    Assert.defined(shallowArrayChange);
+
+    const nestedArrayChange1 = shallowArrayChange[0] as Diff<T[1][0]>["changed"];
+    Assert.defined(nestedArrayChange1);
+    Assert.undefined(nestedArrayChange1[0]);
+
+    const nestedArrayChange2 = nestedArrayChange1[1] as Diff<T[1][0][1]>["changed"];
+    Assert.defined(nestedArrayChange2);
+    Assert.defined(nestedArrayChange2[0]);
+    Assert.equal(1337, nestedArrayChange2[0]);
+  }
+
+  @Fact
+  public "deep array removals"(): void {
+    type T = [number, [[number, number[]]]];
+    const a: T = [69, [[1000, [420]]]];
+    const b: T = [69, [[1000, []]]];
+    const diff = createDiff(a, b);
+    Assert.undefined(diff.changed);
+    Assert.defined(diff.removed);
+    Assert.undefined(diff.removed[0 + 1]);
+    Assert.defined(diff.removed[1 + 1])
+
+    const shallowArrayChange = diff.removed[1 + 1] as Diff<T[1]>["removed"];
+    Assert.notEqual(true, shallowArrayChange);
+    Assert.defined(shallowArrayChange);
+
+    const nestedArrayChange1 = shallowArrayChange[0 + 1] as Diff<T[1][0]>["removed"];
+    Assert.notEqual(true, nestedArrayChange1);
+    Assert.defined(nestedArrayChange1);
+    Assert.undefined(nestedArrayChange1[0 + 1]);
+
+    const nestedArrayChange2 = nestedArrayChange1[1 + 1] as Diff<T[1][0][1]>["removed"];
+    Assert.notEqual(true, nestedArrayChange1);
+    Assert.defined(nestedArrayChange2);
+    Assert.defined(nestedArrayChange2[0 + 1]);
+    Assert.true(nestedArrayChange2[0 + 1]);
+  }
+
+  @Fact
+  public "shallow object changes"(): void {
+    const a = { foo: 123 };
+    const b = { foo: 69 };
+    const diff = createDiff(a, b);
+    Assert.undefined(diff.removed);
+    Assert.defined(diff.changed);
+    Assert.defined(diff.changed.foo);
+    Assert.equal(69, diff.changed.foo);
+  }
+
+  @Fact
+  public "shallow object removals"(): void {
+    const a = { foo: 123 };
+    const b = {};
+    const diff = createDiff(a, b as typeof a);
+    Assert.undefined(diff.changed);
+    Assert.defined(diff.removed);
+    Assert.defined(diff.removed.foo);
+    Assert.true(diff.removed.foo);
+  }
+
+  @Fact
+  public "deep object changes"(): void {
+    const a = { foo: 123, bar: { baz: { n: 69 } } };
+    const b = { foo: 123, bar: { baz: { n: 42 } } };
+    const diff = createDiff(a, b);
+    Assert.undefined(diff.removed);
+    Assert.defined(diff.changed);
+    Assert.undefined(diff.changed.foo);
+    Assert.defined(diff.changed.bar)
+    Assert.defined(diff.changed.bar.baz)
+    Assert.defined(diff.changed.bar.baz)
+    Assert.defined(diff.changed.bar.baz.n);
+    Assert.equal(42, diff.changed.bar.baz.n);
+  }
+
+  @Fact
+  public "deep object removals"(): void {
+    const a = { foo: 123, bar: { baz: { n: 69 } } };
+    const b = { foo: 123, bar: { baz: {} } };
+    const diff = createDiff(a, b as typeof a);
+    Assert.undefined(diff.changed);
+    Assert.defined(diff.removed);
+    Assert.undefined(diff.removed.foo);
+    Assert.defined(diff.removed.bar);
+    Assert.notEqual(true, diff.removed.bar);
+
+    const bar = diff.removed.bar as NonNullable<Diff<typeof a.bar>["removed"]>;
+    Assert.defined(bar.baz);
+    Assert.notEqual(true, bar.baz);
+
+    const baz = bar.baz as NonNullable<Diff<typeof a.bar.baz>["removed"]>;
+    Assert.defined(baz.n);
+    Assert.true(baz.n);
+  }
+}
+
+export = CreateDiffTest;
